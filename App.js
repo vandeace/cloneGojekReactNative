a56210c925149ba80c615197ff160b250abb8390
Muhammad Jafar Ash Siddiq <dbts92@gmail.com>
2020-07-17 13:42:58 +0700

    add search bar

--- App.js
+++ App.js
@@ -7,13 +7,35 @@ import {
   Text,
   StatusBar,
   Image,
+  TextInput,
 } from 'react-native';
 
 const App = () => {
   return (
     <View style={{flex: 1}}>
-      <View style={{flex: 1, backgroundColor: 'pink'}}>
-        <Text>Clonning Gojek</Text>
+      <View style={{flex: 1, backgroundColor: 'white'}}>
+        <View
+          style={{marginHorizontal: 17, flexDirection: 'row', paddingTop: 15}}>
+          <View style={{position: 'relative', flex: 1}}>
+            <TextInput
+              style={styles.inputText}
+              placeholder="What do you want to eat?"
+            />
+            <Image
+              source={require('./src/assets/icon/search.png')}
+              style={{position: 'absolute', top: 8, left: 13}}
+            />
+          </View>
+          <View
+            style={{
+              width: 35,
+              height: 41,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <Image source={require('./src/assets/icon/promo.png')} />
+          </View>
+        </View>
       </View>
       <View style={styles.wrapper}>
         <View style={styles.navItem}>
@@ -80,6 +102,17 @@ const styles = StyleSheet.create({
   iconImage: {
     width: 26,
     height: 26,
+  },
+  inputText: {
+    borderWidth: 1,
+    borderColor: '#e8e8e8',
+    borderRadius: 25,
+    height: 40,
+    fontSize: 13,
+    paddingLeft: 45,
+    paddingRight: 20,
+    backgroundColor: 'white',
+    marginRight: 18,
   },
 });
 
